6f716c8b61e3afe0427ea9ec8ee72820aa18e364
clebershimizu <cleber.shimizu@gmail.com>
2021-02-22 19:01:25 -0300

    initial commit

--- components/footer.js
+++ components/footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
+            Câmara de Inovação de São José dos Pinhais
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
@@ -20,7 +20,7 @@ export default function Footer() {
               href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
               className="mx-3 font-bold hover:underline"
             >
-              View on GitHub
+              Desenvolvido por Code Sails
             </a>
           </div>
         </div>
