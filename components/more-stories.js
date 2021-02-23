66900068d70a989aa1c88a885442d73f9bcf1aae
clebershimizu <cleber.shimizu@gmail.com>
2021-02-23 18:07:10 -0300

    Better layout

--- components/more-stories.js
+++ components/more-stories.js
@@ -2,9 +2,9 @@ import PostPreview from './post-preview'
 
 export default function MoreStories({ posts }) {
   return (
-    <section>
+    <section id='publicacoes'>
       <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
-        Eventos
+        Publicações
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 md:col-gap-16 lg:col-gap-32 row-gap-20 md:row-gap-32 mb-32">
         {posts.map((post) => (
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Layout from '@/components/layout'
 import { getAllPostsForHome } from '@/lib/api'
 import Head from 'next/head'
 import { CMS_NAME } from '@/lib/constants'
+import Navbar from '@/components/navbar'
 
 export default function Index({ allPosts }) {
   const heroPost = allPosts[0]
@@ -13,8 +14,9 @@ export default function Index({ allPosts }) {
   return (
     <>
       <Layout>
+        <Navbar />
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          {/* <title>Next.js Blog Example with {CMS_NAME}</title> */}
         </Head>
         <Container>
           <Intro />
